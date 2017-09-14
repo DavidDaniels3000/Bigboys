1a9877b40c8725c51cb130d386bf836833414076
Wisdom Esset <easset.ee@gmail.com>
2017-09-14 17:43:29 +0100

    Adding form

---
+++ form.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>FillExam</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width:device-width, initial-scale=1">
+		<meta http-equiv="content-type" content="text/html"> <!-- OR <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<link rel="shortcut icon" href="images/blossom.jpg">
+
+		<!--Bootstrap3-->
+
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/w3.css">
+		<link rel="stylesheet" type="text/css" href="form.css">
+		<style type="text/css">
+			.form-control {
+				margin-top: 5px;
+				margin-bottom: 5px;
+			}
+
+			.wd-80, .wd-40 {
+				margin: 5px auto;
+			}
+			.wd-40 {
+				width:  40%;
+			}
+			.wd-80 {
+				width:  80%;
+			}
+
+		</style>
+	</head>
+	<body>
+		<div class="container-fliud"><header class="bg-primary"><p>Wisdom</p></header></div>
+		<div class="container">
+			<p><b>CLASS</b>:
+			<select>
+	        	<option value="">JSS1</option>
+	            <option value="">JSS2</option>
+	            <option value="">JSS3</option>
+	            <option value="">SSS1</option>
+	            <option value="">SSS2</option>
+	            <option value="">SSS3</option>
+		    </select>
+		   </p>
+		   <p><b>TERMS</b>:
+			<select>
+	        	<option value="">First Term</option>
+	            <option value="">Second Term</option>
+	            <option value="">Third Term</option>
+		    </select>
+		   </p>
+		   <br/>
+		   <table class="table-responsive table-hover">
+		   		<thead>
+		   			<th class="text-center">No.</th>
+		   			<th class="text-center">Subject</th>
+		   			<th class="text-center">Test1</th>
+		   			<th class="text-center">Test2</th>
+		   			<th class="text-center">Test3</th>
+		   			<th class="text-center">Examination</th>
+		   			<th class="text-center">Total</th>
+		   			<th class="text-center">Grade</th>
+		   		</thead>
+		   		<tbody>
+		   			<form method="post" action="" >
+				   		<tr>
+				   			<td>1</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>2</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>3</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>4</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>5</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>6</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>7</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>8</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>9</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>10</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>11</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>12</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>13</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>14</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>15</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>16</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>17</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   		    <td>18</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>19</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>20</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>21</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>22</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+				   		<tr>
+				   			<td>23</td>
+				   			<td><input type="text" name="" class="form-control "/></td>
+				   			<td><input type="text" name="" class="form-control text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-40"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   			<td><input type="text" name="" class="form-control  text-center wd-80"/></td>
+				   		</tr>
+			    	</form>
+			   	</tbody>
+		   </table>
+		   <input id="submit" type="submit" value="Submit" class="btn btn-primary" />
+		</div>
+		<footer class="bg-primary"><p>Copyright &copy;</p></footer>
+		<script src="../js/jquery.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+	</body>
+</html>
